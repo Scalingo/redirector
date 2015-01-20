9dc82e6ad1f8a13ebe4f8f770cf0c7e6170ae8bf
Soulou <leo@unbekandt.eu>
2015-01-20 11:11:19 +0100

    Appsdeck dashboard redirection

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 )
@@ -11,10 +12,54 @@ func main() {
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		url := *req.URL
 		url.Scheme = "http"
+		if req.Host == "appsdeck.eu" {
+			redirectAppsdeck(res, req)
+			return
+		}
 		url.Host = strings.Replace(req.Host, "appsdeck.eu", "scalingo.com", 1)
 		http.Redirect(res, req, url.String(), 301)
 	})
 
 	log.Println("listenening on", os.Getenv("PORT"))
 	log.Fatalln(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
+
+var appsdeckRedirections = map[string]string{
+	"#containers-tab":    "",
+	"#logs-tab":          "/logs",
+	"#dns-tab":           "/domains",
+	"#env-tab":           "/variables",
+	"#addons-tab":        "/addons",
+	"#collaborators-tab": "/collaborators",
+	"#deployments-tab":   "/timeline",
+	"#management-tab":    "/settings",
+	"/pricing":           "https://scalingo.com/pricing",
+	"/roadmap":           "http://changelog.scalingo.com",
+	"/contact":           "https://scalingo.com/contact",
+}
+
+func redirectAppsdeck(res http.ResponseWriter, req *http.Request) {
+	path := strings.TrimSpace(strings.TrimPrefix(req.URL.Path, "/home"))
+	pathFragment := path
+	if len(req.URL.Fragment) > 0 {
+		pathFragment += "#" + req.URL.Fragment
+	}
+
+	u := &url.URL{}
+	u.Scheme = "https"
+	for f, p := range appsdeckRedirections {
+		if req.URL.Path == f && strings.HasPrefix(p, "http") {
+			pParsed, _ := url.Parse(p)
+			u.Scheme = pParsed.Scheme
+			u.Host = pParsed.Host
+			u.Path = pParsed.Path
+			break
+		} else if strings.HasPrefix(pathFragment, "/apps") {
+			u.Host = "my.scalingo.com"
+			pathFragment = strings.Replace(pathFragment, f, p, 1)
+			u.Path = pathFragment
+		}
+	}
+
+	http.Redirect(res, req, u.String(), 301)
 }
